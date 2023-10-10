faff5bb608b598b0e8ed849b93449bfbd787d3dd
tiagoJBO <timarao2@hotmail.com>
2023-10-09 23:34:27 -0300

    acresentando if /else

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min) + min)
-    console.log(min, max)
+if(min>=max){
+    alert( "valor Entre tem que ser maior que valor E")
+    }
+
+
+    else{
+        const result = Math.floor(Math.random() * (max - min) + min)   
 
     alert(result)
+    }
 
+    
 }
 
